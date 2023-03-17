396298faa3d89ec097d98e7bba846130b2d3caec
Joseph Musenge <josephmusengep62@gmail.com>
2023-03-16 21:15:13 -0500

    changed layout and added smooth scroll functionality

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,73 @@
 // ALL VARIABLES
+// ********** set date ************
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+// ********** close links ************
 const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
-// NAV BAR TOGGLE
-// Add event listener on toggle button
 navToggle.addEventListener("click", function() {
-    links.classList.toggle("show-links");
+    // linksContainer.classList.toggle("show-links");
+
+    // Set height dyanamically using the getBoundingClientRect() method
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+})
+
+// ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function() {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+
+    
+})
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+        // Prevent default scroll 
+        e.preventDefault();
+
+        // navigate to specific section
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+
+        // Now get position of that element using offsetTop property
+        let position = element.offsetTop - navHeight;
+
+        if(!fixedNav) {
+            position -= navHeight;
+        }
+        if(navHeight > 82) {
+            position += containerHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        // Close toggle once a link is clicked
+        linksContainer.style.height = 0;
+    })
 })
